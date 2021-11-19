4bd4cc4b6ecef924cd063a3bdfd544bae1969bd1
helpMeRise <rises128@gmail.com>
2021-11-19 10:41:24 +0200

    lesson02

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 'use strict';
 
-const itemName = 'Булка';
-const itemQuantity = 10;
-const itemCategoty = 'Еда';
-const itemPrise = 50;
+{
+  const itemName = 'Булка';
+  const itemQuantity = 10;
+  const itemCategoty = 'Еда';
+  const itemPrise = 50;
 
-console.log('itemName: ', itemName);
+  console.log('itemName: ', itemName);
+  console.log(itemPrise * itemQuantity);
+}
 
-// console.log(itemPrise * itemQuantity);
+{
+  const itemName = 'Тортик';
+  const itemQuantity = 3;
+  const itemCategoty = 'Еда';
+  const itemPrise = 250;
+
+  console.log('itemName: ', itemName);
+  console.log(itemPrise * itemQuantity);
+}
