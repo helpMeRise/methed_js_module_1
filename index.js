694e29be060642530974cc6ca38e43acb31cd9a2
helpMeRise <rises128@gmail.com>
2021-11-19 10:36:15 +0200

    lesson01

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const itemName = 'Булка';
+const itemQuantity = 10;
+const itemCategoty = 'Еда';
+const itemPrise = 50;
+
+console.log('itemName: ', itemName);
+
+// console.log(itemPrise * itemQuantity);
