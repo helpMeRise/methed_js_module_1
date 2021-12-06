c40250b2f3886243366e497694a76c0bfad05283
helpMeRise <rises128@gmail.com>
2021-12-06 13:53:30 +0200

    Выполнил задание к 9 уроку

---
+++ lesson09/basket.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const basket = {
+  items: [],
+  totalPrice: 0,
+  count: 0,
+  getTotalPrice() {
+    return this.totalPrice;
+  },
+  add(itemName, itemPrice, itemsCount = 1) {
+    this.items.push({itemName, itemPrice, itemsCount});
+    this.increaeCount(itemsCount);
+    this.totalPrice = 0;
+    this.calculateItemPrice();
+  },
+  increaeCount(number) {
+    this.count += number
+  },
+  calculateItemPrice() {
+    this.items.forEach( item => {
+      this.totalPrice += item.itemPrice * item.itemsCount;
+    } );
+  },
+  clear() {
+    this.items = [],
+    this.totalPrice = 0,
+    this.count = 0
+  },
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(this.totalPrice);
+  },
+};
+
+let goods = +prompt('Сколько товаров хотите добавить в корзину? ')
+
+while (goods > 0) {
+  let itemName = prompt('Название товара'),
+      itemPrice = +prompt('Цена товара'),
+      itemcount = +prompt('Количество товара');
+  goods--;
+
+  basket.add(itemName, itemPrice, itemcount);
+}
+
+basket.print();
